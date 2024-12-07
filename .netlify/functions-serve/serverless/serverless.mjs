7e5b9c6058681df336d6076d1456847da68cc008
daiya122815 <daiya122815@gmail.com>
2024-12-07 18:03:59 +0900

    netlify functionsの導入

---
+++ .netlify/functions-serve/serverless/serverless.mjs
@@ -0,0 +1,20 @@
+
+import {createRequire as ___nfyCreateRequire} from "module";
+import {fileURLToPath as ___nfyFileURLToPath} from "url";
+import {dirname as ___nfyPathDirname} from "path";
+let __filename=___nfyFileURLToPath(import.meta.url);
+let __dirname=___nfyPathDirname(___nfyFileURLToPath(import.meta.url));
+let require=___nfyCreateRequire(import.meta.url);
+
+
+// netlify/functions/serverless.mjs
+var serverless_default = async () => {
+  const data = {
+    message: "Hello, World!"
+  };
+  return new Response(JSON.stringify(data));
+};
+export {
+  serverless_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsibmV0bGlmeS9mdW5jdGlvbnMvc2VydmVybGVzcy5tanMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImV4cG9ydCBkZWZhdWx0IGFzeW5jICgpID0+IHtcclxuICAgIGNvbnN0IGRhdGEgPSB7XHJcbiAgICAgICAgbWVzc2FnZTogXCJIZWxsbywgV29ybGQhXCIsXHJcbiAgICB9O1xyXG4gICAgcmV0dXJuIG5ldyBSZXNwb25zZShKU09OLnN0cmluZ2lmeShkYXRhKSk7XHJcbn0iXSwKICAibWFwcGluZ3MiOiAiOzs7Ozs7Ozs7O0FBQUEsSUFBTyxxQkFBUSxZQUFZO0FBQ3ZCLFFBQU0sT0FBTztBQUFBLElBQ1QsU0FBUztBQUFBLEVBQ2I7QUFDQSxTQUFPLElBQUksU0FBUyxLQUFLLFVBQVUsSUFBSSxDQUFDO0FBQzVDOyIsCiAgIm5hbWVzIjogW10KfQo=
